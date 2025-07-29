74eca50811b57c70d1ef6c033a1ec641514748e0
Maxwell Easter <maxwelleaster00@gmail.com>
2025-07-29 01:08:56 +0100

    update folder

---
+++ desc.js
@@ -0,0 +1,49 @@
+
+/* 
+
+version 1: 
+    -user auth
+    -capsule (create, edit, delete)
+    -file uploads & storage (img, aud, vid, txt)
+    -capsule lock/unlock + timer 
+    -capsule metadata (title, desc, tag, size, created at & unlock date)
+    -dtcapsule wrapped 
+    -analytics dashboard 
+    -settings & notif 
+
+
+version 2:
+    -search & filters (find capsules by title, tag)
+    -share capsules (make capsules for others)
+
+
+version 3:
+    -collaborative capsules (family, friends, teams)
+    -{role-based access}
+
+
+version 4:
+    -public capsules (share capsules with the world)
+    -improved file/media support 
+
+
+version 5: 
+    -custom unlock triggers (location, qr code, shared unlock code)
+
+
+version 5:
+    -capsule backups (downloadable zip folder or google/ms drive)
+
+
+sample use cases: 
+    -end of year reflections
+    -digital will & legacy planning 
+    -journaling 
+    -kids growing up 
+    -letters to future self
+    -weddings & anniversaries
+    -historical archives
+    -family memories 
+    -graduations
+
+*/
